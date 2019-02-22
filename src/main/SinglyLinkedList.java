ba01cb8276d58489a48468a7b0f266c377163874
deeksha kashyap <deekshakas>
2019-02-22 19:26:29 +0530

    implement the insert method ,value and node, int and Node

--- src/main/SinglyLinkedList.java
+++ src/main/SinglyLinkedList.java
@@ -4,23 +4,45 @@ public class SinglyLinkedList {
 
     private Node head=null;
     private int size=0;
+    // method to add a new node at the head
+    public void insert(int value){
+        head=new Node(value, this.head);
+        size++;
+    }
+
+    public void insert(int value, Node node){
+        node.next=new Node(value,node.next);
+        size++;
+    }
+
 
     @Override
     public String toString(){
         StringBuilder result=new StringBuilder();
 
         result.append("[");
-Node temp=this.head;
-while(temp!=null) {
- reult.append(temp.data);
+        Node temp=this.head;
+        while(temp!=null) {
+        result.append(temp.data);
+        if(temp.next!=null)
+ {
+     result.append("==>");
+ }
     temp = temp.next;
 }
         result.append("]");
+
+        return result.toString();
     }
 
     public static void main(String[] args) {
 
-        SinglyLinkedList linkedlist=new SinglyLinkedList();
+        SinglyLinkedList linkedList=new SinglyLinkedList();
+        for (int i = 0; i< 5; i++) {
+            linkedList.insert(i+1);
+        }
+
+
         System.out.println(linkedlist);
     }
 
@@ -32,8 +54,9 @@ while(temp!=null) {
             this.data = data;
             this.next = next;
         }
+
         public Node(int data){
-            this.date=date;
+            this.data=data;
             this.next=null;
         }
 
