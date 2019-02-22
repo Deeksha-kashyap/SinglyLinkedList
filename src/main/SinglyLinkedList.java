07760507e5a89ef61620a7bf587f4f9aba01cbb9
deeksha kashyap <deekshakas>
2019-02-22 09:24:55 +0530

    add var head and size,private Node and private int

--- src/main/SinglyLinkedList.java
+++ src/main/SinglyLinkedList.java
@@ -1,6 +1,11 @@
 package main;
 
 public class SinglyLinkedList {
+
+    private Node head=null;
+    private int size=0;
+
+
     private static class Node{
      private int data;
      private Node next;
