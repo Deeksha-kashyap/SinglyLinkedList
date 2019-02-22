e4e487871430900cbf188707760b19334774ae49
deeksha kashyap <deekshakas>
2019-02-22 09:30:51 +0530

    add toString method

--- src/main/SinglyLinkedList.java
+++ src/main/SinglyLinkedList.java
@@ -5,6 +5,21 @@ public class SinglyLinkedList {
     private Node head=null;
     private int size=0;
 
+    @Override
+    public String toString(){
+        StringBuilder result=new StringBuilder();
+
+        result.append("[");
+
+
+        result.append("]");
+    }
+
+    public static void main(String[] args) {
+
+        SinglyLinkedList linkedlist=new SinglyLinkedList();
+        System.out.println(linkedlist);
+    }
 
     private static class Node{
      private int data;
